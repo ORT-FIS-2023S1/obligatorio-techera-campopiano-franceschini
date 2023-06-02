308c759be586b9ac2df3b6aacffb06e15ece578f
acampopiano <acampopiano@gmail.com>
2023-06-01 22:04:10 -0300

    subo algunos cambios mas, hago la persistencia de los datos del plato en node-cache y agrego sweetalert2

--- src/admin/routes/admin.routes.ts
+++ src/admin/routes/admin.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import path from "path";
+import Cache from "../../utils/cache";
+import { v4 as uuidv4 } from "uuid";
 
 const router = Router();
-
+const cache = Cache.getInstance();
 // Otras rutas de admin
-
+const platos = [];
 router.get("/index", (req, res) => {
   //get user from locals
   const user = res.locals.user;
@@ -13,6 +15,7 @@ router.get("/index", (req, res) => {
     data: {},
     configs: {},
     view: "dashboard",
+    platos: platos,
   });
 });
 
@@ -26,5 +29,23 @@ router.get("/index/agregarPlato", (req, res) => {
     view: "agregarPlato",
   });
 });
-
+router.post("/index/agregarPlato", (req, res) => {
+  const datosId = uuidv4();
+  const datosPlato = new Map();
+  datosPlato.set("nombre", req.body.nombre);
+  datosPlato.set("precio", req.body.precio);
+  datosPlato.set("ingredientes", req.body.ingredientes);
+  datosPlato.set("porcion", req.body.porcion);
+  datosPlato.set("nutricional", req.body.nutricional);
+  cache.set(datosId, datosPlato);
+  const platoObj = Object.fromEntries(datosPlato);
+  platos.push(platoObj);
+  res.sendStatus(200);
+  // Envía una respuesta exitosa al cliente
+});
+cache.on("set", function (key, value) {
+  console.log(`New Key ${key} inserted into cache`);
+  console.log(cache.keys());
+  console.log(cache.getStats());
+});
 export default router;
--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import userRoutes from "./user/routes";
 import adminRoutes from "./admin/routes";
 import sharedRoutes from "./shared/routes";
 import validateToken from "./shared/middlewares/validateToken";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 //app.use(express.static(__dirname + "./shared/public"));
 // Configurar la carpeta 'public' como estática
 app.use(express.static(path.join(__dirname, "shared/public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./shared/interface/views"));
 app.use(
