25c2006b7bd06c8a75b74285764ce45827e856c2
acampopiano <acampopiano@gmail.com>
2023-06-18 13:31:20 -0300

    subimos cambios: testing jest

---
+++ src/tests/Diner.test.ts
@@ -0,0 +1,39 @@
+import Diner from "../shared/domain/entities/Diner";
+
+describe("Diner", () => {
+  let diner: Diner;
+
+  beforeEach(() => {
+    diner = new Diner("Juan", "Perez", "1");
+  });
+
+  it("should correctly initialize Diner instance", () => {
+    expect(diner.name).toBe("Juan");
+    expect(diner.surname).toBe("Perez");
+    expect(diner.getIdentifier()).toBe("1");
+  });
+
+  it("should set name", () => {
+    diner.name = "Pedro";
+    expect(diner.name).toBe("Pedro");
+  });
+
+  it("should set surname", () => {
+    diner.surname = "Rodriguez";
+    expect(diner.surname).toBe("Rodriguez");
+  });
+
+  it("should return identifier as id", () => {
+    const identifier = diner.getIdentifier();
+    expect(identifier).toBe("1");
+  });
+
+  it("should return JSON representation", () => {
+    const json = diner.toJSON();
+    expect(json).toEqual({
+      id: "1",
+      name: "Juan",
+      surname: "Perez",
+    });
+  });
+});
---
+++ src/tests/Group.test.ts
@@ -0,0 +1,67 @@
+import Group from "../shared/domain/entities/Group";
+import Diner from "../shared/domain/entities/Diner";
+
+describe("Group", () => {
+  let group: Group;
+  let diner1: Diner;
+  let diner2: Diner;
+  let diner3: Diner;
+
+  beforeEach(() => {
+    diner1 = new Diner("1", "Pedro", "Gomez");
+    diner2 = new Diner("2", "Carlos", "Perez");
+    diner3 = new Diner("3", "Jose", "Rodriguez");
+    group = new Group(
+      "Grupo 1",
+      "Descripcion grupo",
+      [diner1, diner2],
+      "12345"
+    );
+  });
+
+  it("should correctly initialize Group instance", () => {
+    expect(group.name).toBe("Grupo 1");
+    expect(group.description).toBe("Descripcion grupo");
+    expect(group.members).toEqual([diner1, diner2]);
+    expect(group.getIdentifier()).toBe("12345");
+  });
+
+  it("should set name", () => {
+    group.name = "Grupo actualizado";
+    expect(group.name).toBe("Grupo actualizado");
+  });
+
+  it("should set description", () => {
+    group.description = "Descripcion actualizada";
+    expect(group.description).toBe("Descripcion actualizada");
+  });
+
+  it("should set members", () => {
+    group.members = [diner1, diner2, diner3];
+    expect(group.members).toEqual([diner1, diner2, diner3]);
+  });
+
+  it("should add member", () => {
+    group.addMember(diner3);
+    expect(group.members).toEqual([diner1, diner2, diner3]);
+  });s
+
+  it("should remove member", () => {
+    group.removeMember(diner2);
+    expect(group.members).toEqual([diner1]);
+  });
+
+  it("should return identifier as id", () => {
+    const identifier = group.getIdentifier();
+    expect(identifier).toBe("12345");
+  });
+
+  it("should return JSON representation", () => {
+    const json = group.toJSON();
+    expect(json).toEqual({
+      name: "Grupo 1",
+      description: "Descripcion grupo",
+      members: [diner1, diner2],
+    });
+  });
+});
